9eb9d2b23d43ba006dd53261b434c87d82df2a65
tuhm1 <hmtu117@gmail.com>
2021-08-07 09:34:08 +0700

    merge client server folder

--- src/server.js
+++ src/server.js
@@ -2,5 +2,5 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(express.static(path.join(__dirname, '..', '..', 'client', 'dist')))
+app.use(express.static(path.join(__dirname, '..', 'dist')))
     .listen(PORT, () => console.log(`Listening on port ${PORT}`));
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
 const path = require('path');
 
 module.exports = {
+    entry: './src/client.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
         port: 3000
